6bfaaa299f216fc52f3d82647eaef1bee8b7adb6
riskiputraalamzah <riskialamzah1@gmail.com>
2025-08-05 10:25:24 +0700

    membuat kode logic halaman login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+
+        // Validate the request data
+        $credentials = $request->only('email', 'password');
+        $remember = $request->has('remember'); // akan bernilai true jika dicentang
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        // Attempt to log the user in
+        if (Auth::attempt($credentials, $remember)) {
+            return redirect()->route('dashboard');
+        }
+
+        // If authentication fails, redirect back with an error
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+
+
+Route::middleware(['auth', 'web'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
