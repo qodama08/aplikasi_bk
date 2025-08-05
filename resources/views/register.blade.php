e9e6ee5960a85237e930884d471fd2fc8bf9021c
riskiputraalamzah <riskialamzah1@gmail.com>
2025-08-05 08:40:29 +0700

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,21 +15,22 @@
         <meta name="author" content="CodedThemes">
 
         <!-- [Favicon] icon -->
-        <link rel="icon" href="../assets/images/favicon.svg" type="image/x-icon"> <!-- [Google Font] Family -->
+        <link rel="icon" href="{{ asset('assets/images/favicon.svg') }}" type="image/x-icon">
+        <!-- [Google Font] Family -->
         <link rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
             id="main-font-link">
         <!-- [Tabler Icons] https://tablericons.com -->
-        <link rel="stylesheet" href="../assets/fonts/tabler-icons.min.css">
+        <link rel="stylesheet" href="{{ asset('assets/fonts/tabler-icons.min.css') }}   ">
         <!-- [Feather Icons] https://feathericons.com -->
-        <link rel="stylesheet" href="../assets/fonts/feather.css">
+        <link rel="stylesheet" href="{{ asset('assets/fonts/feather.css') }}">
         <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
-        <link rel="stylesheet" href="../assets/fonts/fontawesome.css">
+        <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome.css') }}">
         <!-- [Material Icons] https://fonts.google.com/icons -->
-        <link rel="stylesheet" href="../assets/fonts/material.css">
+        <link rel="stylesheet" href="{{ asset('assets/fonts/material.css') }}">
         <!-- [Template CSS Files] -->
-        <link rel="stylesheet" href="../assets/css/style.css" id="main-style-link">
-        <link rel="stylesheet" href="../assets/css/style-preset.css">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" id="main-style-link">
+        <link rel="stylesheet" href="{{ asset('assets/css/style-preset.css') }}">
 
     </head>
     <!-- [Head] end -->
@@ -139,12 +140,12 @@
         </div>
         <!-- [ Main Content ] end -->
         <!-- Required Js -->
-        <script src="../assets/js/plugins/popper.min.js"></script>
-        <script src="../assets/js/plugins/simplebar.min.js"></script>
-        <script src="../assets/js/plugins/bootstrap.min.js"></script>
-        <script src="../assets/js/fonts/custom-font.js"></script>
-        <script src="../assets/js/pcoded.js"></script>
-        <script src="../assets/js/plugins/feather.min.js"></script>
+        <script src="{{ asset('assets/js/plugins/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/simplebar.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/fonts/custom-font.js') }}"></script>
+        <script src="{{ asset('assets/js/pcoded.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/feather.min.js') }}"></script>
 
 
 
